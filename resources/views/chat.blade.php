09e46aed13a98663000440049c09d96eed41429f
kk0055 <rtsw.lgy@gmail.com>
2020-10-08 00:01:02 +0800

    until brodcasting

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,11 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Document</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
   <style>
     .list-group{
-      overflow-y:scroll;
+      overflow-y: auto;
       height:200px;
     }
   </style>
@@ -18,15 +20,18 @@
     <div class="row" id="app">
       <div class="offset-4 col-4">
         <li class="list-group-item active">Chat room</li>
-        <ul class="list-group">
+        <ul class="list-group" v-chat-scroll>
       
       <message
       v-for="value in chat.message"
+      :key=value.index
+      color='success'
       >  @{{ value }}</message>
   
-          <input type="text" class="form-control" placeholder="Type your message" v-model='message'
-          @keyup.enter='send'>
+     
         </ul>
+        <input type="text" class="form-control" placeholder="Type your message" v-model='message'
+        @keyup.enter='send'>
       </div>
      
     </div>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('chat',function() {
-    return view('chat');
-});
+Route::get('chat','ChatController@chat');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
